06a895508da801555937a41f611c10893c4e557a
hezou <hezoubeke18@gmail.com>
2025-07-06 23:54:23 +0000

    scrollbar update

--- hezou-beke/src/App.jsx
+++ hezou-beke/src/App.jsx
@@ -4,8 +4,6 @@
 const App = () => {
 
   return <h1>Hello</h1>
-
-
 }
 
 export default App;
--- hezou-beke/tailwind.config.js
+++ hezou-beke/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
+import tailwindScrollbar from "tailwind-scrollbar";
 export default {
   content: [
     "./index.html",
