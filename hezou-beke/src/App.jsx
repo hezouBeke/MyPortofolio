2d2e360d42dc307f605cd32b3c4ae0504753cb86
hezou <hezoubeke18@gmail.com>
2025-07-08 10:35:44 +0000

    update

--- hezou-beke/src/App.jsx
+++ hezou-beke/src/App.jsx
@@ -4,8 +4,30 @@ import Header from "./components/Header";
 const App = () => {
 
   return(
-    <header className="fixed top-0 left-0 w-full">
+    <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
+    <div className="max-w-screen-2xl w-full mx-auto px-4">
+    <h1>
+      <a href="/" className="logo">
+      <img src="/images/logo.svg" 
+      width={40}
+      height={40}
+      alt=" hezou beke" />
+      
+      </a>
+    </h1>
 
+    <div className="">
+    <button className=""
+    onClick={null}
+    >
+      <span className="material-symbols-rounded">
+      menu
+      </span> 
+    </button>
+  
+
+    </div>
+    </div>
     </header>
   )
 }
