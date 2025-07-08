5747afb53cad57c6fc01adcfafb75af5683afa47
hezou <hezoubeke18@gmail.com>
2025-07-08 22:56:13 +0000

    update

--- hezou-beke/src/components/Header.jsx
+++ hezou-beke/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
    <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
-    <div className="max-w-screen-2xl w-full mx-auto px-4">
+    <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center">
     <h1>
       <a href="/" className="logo">
       <img src="/images/logo.svg" 
@@ -22,9 +22,12 @@ const Header = () => {
       menu
       </span> 
     </button>
+    Navbar
   
-
     </div>
+     <a href="#contact" className="">
+      Contact Me
+    </a>
     </div>
     </header>
   )
