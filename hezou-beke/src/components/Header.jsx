1cb1128d9bd3640686fa9dbbed32569e500f8a8a
hezou <hezoubeke18@gmail.com>
2025-07-22 21:58:34 +0000

    update

--- hezou-beke/src/components/Header.jsx
+++ hezou-beke/src/components/Header.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Navbar from './Navbar'
 import { useState } from 'react'
 const Header = () => {
+
+  const [navOpen, setNavOpen] = useState(false);
+
   return (
    <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
     <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
@@ -23,7 +26,7 @@ const Header = () => {
       menu
       </span> 
     </button>
-    <Navbar navOpen={}/>
+    <Navbar navOpen={navOpen}/>
   
     </div>
      <a href="#contact" className="btn btn-secondary max-md:hidden md:justify-self-end">
