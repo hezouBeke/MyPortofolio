4c6fe5eb9bd718e50c8c1daee91b94adc6031367
hezou <hezoubeke18@gmail.com>
2025-07-30 21:59:42 +0000

    update

--- hezou-beke/src/components/Navbar.jsx
+++ hezou-beke/src/components/Navbar.jsx
@@ -14,44 +14,44 @@ const Navbar = ({ navOpen }) => {
     }
     useEffect(initActiveBox, []);
     window.addEventListener('resize', initActiveBox)
-  const activeCurrentLink = (event) => {
-        lastActiveLink.current?.classList.remove('active');
-        event.target.classList.add('active');
-        lastActiveLink.current = event.target;
+//   const activeCurrentLink = (event) => {
+//         lastActiveLink.current?.classList.remove('active');
+//         event.target.classList.add('active');
+//         lastActiveLink.current = event.target;
 
-       activeBox.current.style.top = event.target.offsetTop + 'px';
-       activeBox.current.style.left = event.target.offsetLeft + 'px';
-       activeBox.current.style.width = event.target.offsetWidth + 'px';
-       activeBox.current.style.height = event.target.offsetHeight + 'px';
-    }
-    const navItems = [
-        {
-            label: 'Home',
-            link: '#home',
-            className: 'nav-link active',
-            ref: lastActiveLink
-        },
-        {
-            label: 'About',
-            link: '#about',
-            className: 'nav-link'
-        },
-        {
-            label: 'Work',
-            link: '#work',
-            className: 'nav-link'
-        },
-        {
-            label: 'Reviews',
-            link: '#reviews',
-            className: 'nav-link'
-        },
-        {
-            label: 'Contact',
-            link: '#contact',
-            className: 'nav-link md:hidden'
-        }
-    ];
+//        activeBox.current.style.top = event.target.offsetTop + 'px';
+//        activeBox.current.style.left = event.target.offsetLeft + 'px';
+//        activeBox.current.style.width = event.target.offsetWidth + 'px';
+//        activeBox.current.style.height = event.target.offsetHeight + 'px';
+//     }
+//     const navItems = [
+//         {
+//             label: 'Home',
+//             link: '#home',
+//             className: 'nav-link active',
+//             ref: lastActiveLink
+//         },
+//         {
+//             label: 'About',
+//             link: '#about',
+//             className: 'nav-link'
+//         },
+//         {
+//             label: 'Work',
+//             link: '#work',
+//             className: 'nav-link'
+//         },
+//         {
+//             label: 'Reviews',
+//             link: '#reviews',
+//             className: 'nav-link'
+//         },
+//         {
+//             label: 'Contact',
+//             link: '#contact',
+//             className: 'nav-link md:hidden'
+//         }
+//     ];
 
     return (
         <div>
